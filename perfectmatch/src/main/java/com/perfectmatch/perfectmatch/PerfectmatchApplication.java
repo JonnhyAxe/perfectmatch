73c328311d7fb49b5eaa473ac8822e4e0bf663bf
bld_jmsmachado <joaomiguelmachado@boldint.com>
2017-09-29 14:18:37 +0100

    PM: project spring initializr
    Change-Id: I35dc568ff9b97e9c7e8f46cb839df1f39a8cb4a1

---
+++ perfectmatch/src/main/java/com/perfectmatch/perfectmatch/PerfectmatchApplication.java
@@ -0,0 +1,12 @@
+package com.perfectmatch.perfectmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectmatchApplication.class, args);
+	}
+}
---
+++ perfectmatch/src/test/java/com/perfectmatch/perfectmatch/PerfectmatchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.perfectmatch.perfectmatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerfectmatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
