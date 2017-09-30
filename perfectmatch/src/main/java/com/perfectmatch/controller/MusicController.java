932b2fb19439f3a8b15982f12f134db0e0919e56
bld_jmsmachado <joaomiguelmachado@boldint.com>
2017-09-30 11:52:13 +0100

    PM: activate HTTP message converters
    Change-Id: Ie49c7ec3cf87e3c60c9f3f8414504ddf02c70501

--- perfectmatch/src/main/java/com/perfectmatch/controller/MusicController.java
+++ perfectmatch/src/main/java/com/perfectmatch/controller/MusicController.java
@@ -4,16 +4,14 @@ package com.perfectmatch.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.perfectmatch.persistence.dao.MusicRepository;
 import com.perfectmatch.persistence.model.Music;
 
-//@RestController
-
-@Controller
+@RestController
 @RequestMapping("/music")
 public class MusicController {
 
--- perfectmatch/src/main/java/com/perfectmatch/spring/PerfectMatchWebConfig.java
+++ perfectmatch/src/main/java/com/perfectmatch/spring/PerfectMatchWebConfig.java
@@ -1,17 +1,45 @@
 package com.perfectmatch.spring;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
- * <class description>
  *
  */
 
 @Configuration
 @ComponentScan({ "com.perfectmatch.controller" })
-@EnableWebMvc
-public class PerfectMatchWebConfig {
+public class PerfectMatchWebConfig extends WebMvcConfigurationSupport {
 
+    @Bean
+    public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        // Optional<HttpMessageConverter> converter =
+        // converters.stream().filter(converter -> converter instanceof
+        // MappingJackson2HttpMessageConverter).findFirst();
+
+        converters.add(customJackson2HttpMessageConverter());
+        super.addDefaultHttpMessageConverters(converters);
+    }
 }
