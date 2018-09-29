412f754984f929f9e0c8ea7cecfaff9270129fc4
JonnhyAxe <joao_m_s_m@hotmail.com>
2018-09-29 20:42:37 +0100

    PM: add end-2-end test to PerfectMatch.vue (BDD DSL)

--- perfectmatch-vue/test/e2e/specs/PerfectMatchAcceptance.test.js
+++ perfectmatch-vue/test/e2e/specs/PerfectMatchAcceptance.test.js
@@ -30,6 +30,21 @@ module.exports = {
       .assert.containsText('tbody tr:nth-child(3) > td:nth-child(2)', 'Please Stop (Original Mix)XPTO')
       .assert.containsText('tbody tr:nth-child(3) > td:nth-child(3)', 'Show Details')
 
+      // TODO : why does not work (BDD style DSL)
+      // expect element  to be present in 1000ms
+      // .expect.element('#app').to.be.present.before(10000)
+      // expect element <#lst-ib> to have css property 'display'
+      // .expect.element('#app').to.be.visible
+      // .expect.element('#app').to.have.css('margin-left: 50px; margin-right: 50px')
+
+      // .expect.element('.table').to.be.present.before(5000)
+      // .expect.element('.table').to.be.visible
+
+      // .expect.element('div:nth-child(2)').to.have.attribute('class').which.contains('service')
+
+      // .expect.element('thead tr:nth-child(1) > th:nth-child(1)').which.contains('Artist')
+      // .expect.element('thead tr:nth-child(1) > th:nth-child(2)').which.contains('Name')
+      // .expect.element('thead tr:nth-child(1) > th:nth-child(3))').which.contains('Show Details')
       .end()
   }
 }
