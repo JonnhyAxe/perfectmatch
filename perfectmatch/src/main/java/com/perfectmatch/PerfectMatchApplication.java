07a60b763a31787c4784703d10bc864f59652c5c
JonnhyAxe <joao_m_s_m@hotmail.com>
2018-09-30 04:21:40 +0100

    PM: Add parent pom and revert spring to 2.0.4.RELEASE

--- perfectmatch/src/main/java/com/perfectmatch/PerfectMatchApplication.java
+++ perfectmatch/src/main/java/com/perfectmatch/PerfectMatchApplication.java
@@ -3,7 +3,7 @@ package com.perfectmatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 //import com.perfectmatch.spring.PerfectMatchPersistenceJpaConfig;
