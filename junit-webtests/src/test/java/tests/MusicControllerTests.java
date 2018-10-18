23c0b50fda38ba24820330dc124f50c00e5fdc65
JonnhyAxe <joao_m_s_m@hotmail.com>
2018-10-18 21:41:38 +0100

    PM: Add BDD Rest Controller tests

---
+++ junit-webtests/src/test/java/tests/MusicControllerTests.java
@@ -0,0 +1,61 @@
+package tests;
+
+
+import java.net.URLEncoder;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import steps.MusicControllerByNameSteps;
+import steps.MusicControllerRepositorySteps;
+
+
+@RunWith(SerenityRunner.class)
+public class MusicControllerTests {
+   
+	@Steps
+	MusicControllerRepositorySteps musicControllerRepositorySteps;
+
+	@Steps
+	MusicControllerByNameSteps musicControllerByNameSteps;
+	
+	@Test
+	public void verifyThatWeCanFindLatmunPleaseStopMusicInTheRepository(){
+		musicControllerRepositorySteps.searchMusics();
+		musicControllerRepositorySteps.searchIsExecutedSuccesfully();
+		musicControllerRepositorySteps.shouldContainsIdForMusicNumber("5bc8e3b6f0139411c4a79597", 0);
+		musicControllerRepositorySteps.shouldContainsArtistNameForMusicNumber("Latmun", 0);
+		musicControllerRepositorySteps.shouldContainsMusicNameForMusicNumber("Please Stop (Original Mix)", 0);
+	}
+   
+	@Test
+	public void verifyThatWeCanFindLatmunDefMusicInTheRepository(){
+		musicControllerRepositorySteps.searchMusics();
+		musicControllerRepositorySteps.searchIsExecutedSuccesfully();
+		musicControllerRepositorySteps.shouldContainsIdForMusicNumber("5bc8e3b6f0139411c4a79598", 1);
+		musicControllerRepositorySteps.shouldContainsArtistNameForMusicNumber("Latmun", 1);
+		musicControllerRepositorySteps.shouldContainsMusicNameForMusicNumber("def (Original Mix)", 1);
+	}
+   
+	@Test
+    public void verifyThatWeCanFindLatmunPleaseStopXPTOMusicInTheRepository(){
+		musicControllerRepositorySteps.searchMusics();
+		musicControllerRepositorySteps.searchIsExecutedSuccesfully();
+		musicControllerRepositorySteps.shouldContainsIdForMusicNumber("5bc8e3b6f0139411c4a79599", 2);
+		musicControllerRepositorySteps.shouldContainsArtistNameForMusicNumber("LatmunXPTO", 2);
+		musicControllerRepositorySteps.shouldContainsMusicNameForMusicNumber("Please Stop (Original Mix)XPTO", 2);
+    }
+    
+    
+    @Test
+    @Ignore
+    public void verifyThatWeCanFindLatmunPleaseStopXPTOMusicByName(){
+    	musicControllerByNameSteps.searchMusicByName(URLEncoder.encode("Please Stop (Original Mix)XPTO") );
+    	musicControllerByNameSteps.searchIsExecutedSuccesfully();
+    	musicControllerByNameSteps.shouldContainsIdForMusicNumber("5bc8e3b6f0139411c4a79599");
+    	musicControllerByNameSteps.shouldContainsArtistNameForMusicNumber("Latmun");
+    }
+}
