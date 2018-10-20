147349b3b1ba44eec76d1037a63d32f9a00ef540
JonnhyAxe <joao_m_s_m@hotmail.com>
2018-10-20 22:30:58 +0100

    PM: Fix Step mapping

--- perfectmatch/src/test/java/com/perfectmatch/perfectmatch/persistence/dao/feature/steps/MusicDaoRepositorySteps.java
+++ perfectmatch/src/test/java/com/perfectmatch/perfectmatch/persistence/dao/feature/steps/MusicDaoRepositorySteps.java
@@ -1,6 +1,8 @@
 package com.perfectmatch.perfectmatch.persistence.dao.feature.steps;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import com.perfectmatch.persistence.dao.MusicRepository;
 import com.perfectmatch.persistence.model.Music;
@@ -8,10 +10,6 @@ import com.perfectmatch.persistence.model.Music;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNotNull;
 
 public class MusicDaoRepositorySteps {
 	
--- perfectmatch/src/test/resources/features/persistence/search_musics/music_repository.feature
+++ perfectmatch/src/test/resources/features/persistence/search_musics/music_repository.feature
@@ -1,8 +1,8 @@
 Feature: Search musics in the DAO Mongo repository
   I need to be able show the repository of musics
 
-  Scenario: Search for Latmun - Please Stop (Original Mix)
+  Scenario: Search for Latmun - Please Stop (Original Mix) in Music Dao
     Given music repository
     When I get music with name 'Please Stop (Original Mix)'
-    Then music should contain artist name 'Latmun'
+    Then music should contains artist name 'Latmun'
     Then music should contain id
