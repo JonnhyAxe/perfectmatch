fc036b539324da44536c50e5edf203e4dd619c17
JonnhyAxe <joao_m_s_m@hotmail.com>
2018-10-29 21:57:02 +0000

    PM: Upda javascrip Unit Tests

--- perfectmatch-vue/test/unit/components/PerfectMatchData.test.js
+++ perfectmatch-vue/test/unit/components/PerfectMatchData.test.js
@@ -1,42 +1,51 @@
-// import PerfectMatch from '@/components/PerfectMatch'
+import PerfectMatch from '@/components/PerfectMatch'
 import { shallowMount } from '@vue/test-utils'
-import Vue from 'vue'
 
-// import bTable from 'bootstrap-vue/es/components/table/table';
-// Vue.component('b-table', bTable);
 
 describe('PerfectMatch.vue', () => {
   let cmp
 
   beforeEach(() => {
     cmp = shallowMount(PerfectMatch, {
-      data: {
-        musics: [
-          {
-            'id': '5baf59c9f0139422206e6411',
-            'artist': 'Latmun',
-            'name': 'Please Stop (Original Mix)',
-            'style': 'TECH_HOUSE',
-            'samples': [
-              {
-                'id': '5baf59c9f0139422206e640f',
-                'name': 'Latmun:Please Stop (Original Mix)',
-                'timestamp': 180,
-                'mathes': [
-                  {
-                    'id': '5baf59c9f0139422206e640e',
-                    'name': 'Latmun:Please Stop (Original Mix)'
-                  }
-                ]
-              }
-            ]
-          }
-        ]
+      data () {
+        return {
+          musics: [
+            {
+              'id': '5baf59c9f0139422206e6411',
+              'artist': 'Latmun',
+              'name': 'Please Stop (Original Mix)',
+              'style': 'TECH_HOUSE',
+              'samples': [
+                {
+                  'id': '5baf59c9f0139422206e640f',
+                  'name': 'Latmun:Please Stop (Original Mix)',
+                  'timestamp': 180,
+                  'mathes': [
+                    {
+                      'id': '5baf59c9f0139422206e640e',
+                      'name': 'Latmun:Please Stop (Original Mix)'
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
       }
     })
   })
 
-  it.skip('equals messages to predefined music', () => {
-    expect(cmp.vm.musics).toEqual([])
+  it('equals tables items to predefined array', () => {
+    expect(cmp.vm.musics).toEqual([{"artist": "Latmun", "id": "5baf59c9f0139422206e6411", "name": "Please Stop (Original Mix)", "samples": [{"id": "5baf59c9f0139422206e640f", "mathes": [{"id": "5baf59c9f0139422206e640e", "name": "Latmun:Please Stop (Original Mix)"}], "name": "Latmun:Please Stop (Original Mix)", "timestamp": 180}], "style": "TECH_HOUSE"}])
+  })
+  
+  it.skip('should render empty table', () => {
+    console.log(cmp);
+    
+    const table = cmp.find('b-table')
+    expect(table.attributes('fields')).toEqual('artist,name,show_details')
+    expect(table.attributes().fields).toBe('artist,name,show_details')
+    expect(table.text()).toEqual('')
+    expect(table.classes()).toEqual([])
   })
 })
