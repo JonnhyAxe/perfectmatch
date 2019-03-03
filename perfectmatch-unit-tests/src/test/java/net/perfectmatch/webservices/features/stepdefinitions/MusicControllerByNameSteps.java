5464252387b96468ea06ab0c30f4249c959cd8df
JonnhyAxe <joao_m_s_m@hotmail.com>
2019-03-03 23:08:40 +0000

    MF: Fix BDD Tests

--- perfectmatch-unit-tests/src/test/java/net/perfectmatch/webservices/features/stepdefinitions/MusicControllerByNameSteps.java
+++ perfectmatch-unit-tests/src/test/java/net/perfectmatch/webservices/features/stepdefinitions/MusicControllerByNameSteps.java
@@ -7,33 +7,27 @@ import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
 
 public class MusicControllerByNameSteps {
-	
-   private String ISO_CODE_SEARCH = "http://localhost:8081/music/";
-   private Response response;
 
-   
-   @Step
-   public void searchMusicByName(String name){
-       response = SerenityRest.when().get(ISO_CODE_SEARCH + name);
-   }
+  private String ISO_CODE_SEARCH = "http://localhost:8081/music/";
+  private Response response;
 
-   @Step
-   public void searchIsExecutedSuccesfully(){
-       response.then().statusCode(200);
-   }
+  @Step
+  public void searchMusicByName(String name) {
+    response = SerenityRest.when().get(ISO_CODE_SEARCH + name);
+  }
 
-   
-   @Step
-   public void shouldContainsIdForMusicNumber(String id){
-       response.then().body("id", is(id));
-   }
-   
-   
-   @Step
-   public void shouldContainsArtistNameForMusicNumber(String name){
-       response.then().body("artist", is(name));
-   }
-   
+  @Step
+  public void searchIsExecutedSuccesfully() {
+    response.then().statusCode(200);
+  }
 
- 
+  @Step
+  public void shouldContainsIdForMusicNumber(String id) {
+    response.then().body("id", is(id));
+  }
+
+  @Step
+  public void shouldContainsArtistNameForMusicNumber(String name) {
+    response.then().body("artist", is(name));
+  }
 }
---
+++ perfectmatch/src/test/java/com/perfectmatch/perfectmatch/persistence/dao/feature/MusicRepositoryIntegrationTests.java
@@ -0,0 +1,39 @@
+
+package com.perfectmatch.perfectmatch.persistence.dao.feature;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+/**
+ * Integration Test between JPA and Persistence modules
+ *
+ */
+//@RunWith(SpringIntegrationSerenityRunner.class) //does not produce report inside index
+@RunWith(CucumberWithSerenity.class) //TODO does not autowire and do not run an server, only wires beans under the path
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+@CucumberOptions(
+  features = "src/test/resources/features/persistence/search_musics/music_repository.feature"
+)
+@ContextConfiguration(classes = {PerfectMatchServiceConfig.class })
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@EnableMongoRepositories(basePackages = {"com.perfectmatch.persistence"})
+//@ComponentScan({"com.perfectmatch.perfectmatch.persistence.dao.feature.steps"})
+
+//this loads two contexts (backend service  @SpringBootTest AND SpringIntegrationSerenityRunner)
+//the SpringIntegrationSerenityRunner does not scan application dependencies,
+//they are only autowired in the current class  try to remove @SpringBootTest and add @DataMongoTest
+//Refactor this to use pure web test. Note: use Controller tests 
+@Ignore
+public class MusicRepositoryIntegrationTests {
+
+
+
+
+}
