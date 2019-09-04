3209bb5d70d1d2b04772efb4cd9e74a75b3b11be
JonnhyAxe <joao_m_s_m@hotmail.com>
2019-09-04 22:45:56 +0100

    PM: Enable naming server registry in the perfectmactch webapp

--- perfectmatch/src/main/java/com/perfectmatch/PerfectMatchApplication.java
+++ perfectmatch/src/main/java/com/perfectmatch/PerfectMatchApplication.java
@@ -1,15 +1,17 @@
 package com.perfectmatch;
 
-import com.perfectmatch.spring.PerfectMatchWebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
+import com.perfectmatch.spring.PerfectMatchWebConfig;
 
 @SpringBootApplication
 @Import({PerfectMatchWebConfig.class})
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+@EnableDiscoveryClient
 public class PerfectMatchApplication {
 
   public static void main(String[] args) {
