d530bc8a4c980e7f814824ef961847424d86fbd2
JonnhyAxe <joao_m_s_m@hotmail.com>
2019-09-12 19:19:37 +0100

    PM: enable Naming Discovery in Perfect Match

--- perfectmatch/src/main/java/com/perfectmatch/PerfectMatchApplication.java
+++ perfectmatch/src/main/java/com/perfectmatch/PerfectMatchApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 import com.perfectmatch.spring.PerfectMatchWebConfig;
 
@@ -12,6 +13,7 @@ import com.perfectmatch.spring.PerfectMatchWebConfig;
 @Import({PerfectMatchWebConfig.class})
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableFeignClients("com.perfectmatch.common.services.proxy")
 public class PerfectMatchApplication {
 
   public static void main(String[] args) {
